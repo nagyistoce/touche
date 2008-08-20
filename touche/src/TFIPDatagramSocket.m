b2da8bf1dc3e00d6a5d306feac69baf7eb053a06
gkaindl <gkaindl@04bfa92c-8654-0410-99b0-a9ef281f400a>
2008-08-20 15:57:57 +0000

    Provided Objc-C wrappers for BSD sockets as a first step towards OSC/TUIO support (#1) and possibly as a foundation for other networking protocols in the future.

---
+++ touche/src/TFIPDatagramSocket.m
@@ -0,0 +1,328 @@
+//
+//  TFIPDatagramSocket.m
+//  Touché
+//
+//  Created by Georg Kaindl on 18/8/08.
+//
+//  Copyright (C) 2008 Georg Kaindl
+//
+//  This file is part of Touché.
+//
+//  Touché is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  Touché is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with Touché. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "TFIPDatagramSocket.h"
+
+
+@interface TFIPDatagramSocket (PrivateMethods)
+- (BOOL)_receiveDatagrams;
+- (BOOL)_sendDatagrams;
+@end
+
+
+@implementation TFIPDatagramSocket
+
+- (id)init
+{
+	if (nil != (self = [super init])) {
+		_inQueue = [[NSMutableDictionary alloc] init];
+	}
+	
+	return self;
+}
+
+- (void)dealloc
+{
+	[_inQueue release];
+	_inQueue = nil;
+	
+	[_outQueue release];
+	_outQueue = nil;
+	
+	[super dealloc];
+}
+
+- (void)setDelegate:(id)newDelegate
+{
+	[super setDelegate:newDelegate];
+	
+	_datagramDelegateCapabilities.delegateHasReadWriteError =
+		[delegate respondsToSelector:@selector(socketHadReadWriteError:)];
+}
+
+- (BOOL)isConnectionOriented
+{
+	return NO;
+}
+
+- (BOOL)connectTo:(in_addr_t)addr port:(in_port_t)port
+{
+	_socketConnected = ([super connectTo:addr port:port]);	
+	[_inQueue removeAllObjects];
+	
+	return _socketConnected;
+}
+
+- (BOOL)setPeer:(NSString*)name onPort:(in_port_t)port
+{
+	if (0 == port)
+		return NO;
+	
+	if (![self cfSocketCreated])
+		if (![self open])
+			return NO;
+	
+	in_addr_t addr = htonl(INADDR_ANY);
+	if (nil != name && ![self resolveName:name intoAddress:&addr])
+		return NO;
+	
+	return [self connectTo:addr port:port];
+}
+
+- (size_t)recvfrom:(void*)bytes length:(size_t)length endpoint:(NSData**)sockAddr
+{
+	size_t rv = 0;
+	
+	if (0 <= [_inQueue count]) {
+		NSMutableData* inBuffer;
+		NSData* sA;
+		
+		if (NULL != sockAddr) {
+			inBuffer = [_inQueue objectForKey:*sockAddr];
+		}
+		
+		if (nil == inBuffer) {
+			sA = [[[_inQueue allKeys] objectAtIndex:0] retain];
+			inBuffer = [_inQueue objectForKey:sA];
+		} else
+			sA = [*sockAddr retain];
+		
+		size_t amountToRead = MIN(length, [inBuffer length]);
+		[inBuffer getBytes:bytes length:amountToRead];
+		[inBuffer replaceBytesInRange:NSMakeRange(0, amountToRead) withBytes:NULL length:0];
+		
+		if (0 >= [inBuffer length])
+			[_inQueue removeObjectForKey:sA];
+		
+		if (NULL != sockAddr)
+			*sockAddr = sA;
+		
+		_availDataCount -= amountToRead;
+		
+		[sA autorelease];
+		rv = amountToRead;
+	}
+	
+	return rv;
+}
+
+- (void)sendto:(const void*)bytes length:(size_t)length endpoint:(NSData*)sockAddr
+{
+	if (NULL != bytes && 0 < length && (NULL != sockAddr || [self isConnected])) {
+		if (nil == _outQueue)
+			_outQueue = [[NSMutableData alloc] init];
+	
+		NSMutableData* outBuffer = [_outQueue objectForKey:sockAddr];
+		if (nil == outBuffer) {
+			outBuffer = [[NSMutableData alloc] initWithCapacity:length];
+			[_outQueue setObject:outBuffer forKey:sockAddr];
+			[outBuffer release];
+		}
+		
+		[outBuffer appendBytes:bytes length:length];
+		
+		(void)[self _sendDatagrams];
+	}
+}
+
+- (NSData*)receiveDataFromEndpoint:(NSData**)sockAddr
+{
+	return [self receiveDataOfLength:(size_t)[self availableBytes] fromEndpoint:sockAddr];
+}
+
+- (NSData*)receiveDataOfLength:(size_t)length fromEndpoint:(NSData**)sockAddr
+{
+	NSData* data = nil;
+	
+	if (0 < length) {
+		size_t amountToRead = MIN(length, [self availableBytes]);
+		void* buf = alloca(amountToRead);
+		
+		if (NULL != buf) {
+			size_t amountRead = [self recvfrom:buf length:amountToRead endpoint:sockAddr];
+			data = [NSData dataWithBytes:buf length:amountRead];
+		}
+	}
+	
+	return data;
+}
+
+- (size_t)receiveOntoData:(NSMutableData*)data fromEndpoint:(NSData**)sockAddr
+{
+	return [self receiveOntoData:data length:[self availableBytes] fromEndpoint:sockAddr];
+}
+
+- (size_t)receiveOntoData:(NSMutableData*)data length:(size_t)length fromEndpoint:(NSData**)sockAddr
+{
+	size_t beforeLength = [data length];
+	[data appendData:[self receiveDataOfLength:length fromEndpoint:sockAddr]];
+	return [data length] - beforeLength;
+}
+
+- (NSString*)receiveStringFromEndpoint:(NSData**)sockAddr
+{
+	return [self receiveStringWithEncoding:NSUTF8StringEncoding fromEndpoint:sockAddr];
+}
+
+- (NSString*)receiveStringWithEncoding:(NSStringEncoding)encoding fromEndpoint:(NSData**)sockAddr
+{
+	NSString* string = nil;
+	NSData* data = [self receiveDataFromEndpoint:sockAddr];
+	if (nil != data)
+		string = [[[NSString alloc] initWithData:data encoding:encoding] autorelease];
+	return string;
+}
+
+- (size_t)receiveOntoString:(NSMutableString*)str fromEndpoint:(NSData**)sockAddr
+{
+	return [self receiveOntoString:str encoding:NSUTF8StringEncoding fromEndpoint:sockAddr];
+}
+
+- (size_t)receiveOntoString:(NSMutableString*)str encoding:(NSStringEncoding)encoding fromEndpoint:(NSData**)sockAddr
+{
+	size_t beforeLength = [str length];
+	[str appendString:[self receiveStringWithEncoding:encoding fromEndpoint:sockAddr]];
+	return [str length] - beforeLength;
+}
+
+- (void)sendData:(NSData*)data toEndpoint:(NSData*)sockAddr
+{
+	[self sendto:[data bytes] length:[data length] endpoint:sockAddr];
+}
+
+- (void)sendString:(NSString*)string toEndpoint:(NSData*)sockAddr
+{
+	[self sendString:string encoding:NSUTF8StringEncoding toEndpoint:sockAddr];
+}
+
+- (void)sendString:(NSString*)string encoding:(NSStringEncoding)encoding toEndpoint:(NSData*)sockAddr
+{
+	[self sendData:[string dataUsingEncoding:encoding] toEndpoint:sockAddr];
+}
+
+- (void)handleConnectCallbackWithData:(const void*)data
+{
+	// we will never get this callback on a datagram socket, so do nothing
+}
+
+- (void)handleReadCallbackWithData:(const void*)data
+{
+	[self handleAvailableData];
+}
+
+- (void)handleWriteCallbackWithData:(const void*)data
+{
+	[self handleWritableState];
+}
+
+- (void)handleAvailableData
+{
+	BOOL dataRead = [self _receiveDatagrams];
+	
+	if (dataRead && _delegateCapabilities.delegateHasDataAvailable)
+		[delegate socket:self dataIsAvailableWithLength:_availDataCount];
+}
+
+- (void)handleWritableState
+{
+	(void)[self _sendDatagrams];
+}
+
+- (void)handleSocketError
+{
+	if (_datagramDelegateCapabilities.delegateHasReadWriteError)
+		[delegate socketHadReadWriteError:self];
+}
+
+- (BOOL)_receiveDatagrams
+{
+	BOOL success = NO;
+	size_t availableBytes = [self availableBytes];
+	
+	if (availableBytes > 0) {
+		struct sockaddr_in sockAddr;
+		void* bytes = alloca(availableBytes);
+		
+		if (NULL != bytes) {
+			int bytesRead = [self receiveIntoBytes:bytes
+											length:availableBytes
+									  fromSockAddr:&sockAddr];
+			
+			
+			if (0 < bytesRead) {
+				success = YES;
+				_availDataCount += _availDataCount;
+				
+				NSData* sA = [NSData dataWithBytes:&sockAddr length:sizeof(struct sockaddr_in)];
+				NSMutableData* inBuffer = [_inQueue objectForKey:sA];
+				if (nil == inBuffer) {
+					inBuffer = [[NSMutableData alloc] initWithCapacity:bytesRead];
+					[_inQueue setObject:inBuffer forKey:sA];
+					[inBuffer release];
+				}
+				
+				[inBuffer appendBytes:bytes length:(NSUInteger)bytesRead];
+			} else if (0 > bytesRead) {
+				if (EAGAIN != _lastErrorCode && EWOULDBLOCK != _lastErrorCode)
+					[self handleSocketError];
+			}
+		}
+	}
+	
+	return success;
+}
+
+- (BOOL)_sendDatagrams
+{
+	BOOL success = NO;
+	
+	if (0 < [_outQueue count]) {
+		NSData* sA = [[_outQueue allKeys] objectAtIndex:0];
+		NSMutableData* outBuffer = [_outQueue objectForKey:sA];
+		
+		int amountSent = [self sendBytes:[outBuffer bytes]
+								  length:[outBuffer length]
+							  toSockAddr:(const struct sockaddr_in*)[sA bytes]];
+		
+		if (0 <= amountSent) {
+			success = YES;
+			
+			if (0 < amountSent) {
+				[outBuffer replaceBytesInRange:NSMakeRange(0, amountSent) withBytes:NULL length:0];
+				if (0 >= [outBuffer length])
+					[_outQueue removeObjectForKey:sA];
+				
+				if (_delegateCapabilities.delegateHasDataSent)
+					[delegate socket:self didSendDataOfLength:(NSUInteger)amountSent];
+			}
+		} else {
+			if (EWOULDBLOCK != _lastErrorCode)
+				[self handleSocketError];
+		}
+	}
+	
+	return success;
+}
+
+@end
---
+++ touche/src/TFIPSocket.m
@@ -0,0 +1,404 @@
+//
+//  TFIPSocket.m
+//  Touché
+//
+//  Created by Georg Kaindl on 18/8/08.
+//
+//  Copyright (C) 2008 Georg Kaindl
+//
+//  This file is part of Touché.
+//
+//  Touché is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  Touché is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with Touché. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "TFIPSocket.h"
+
+#import <arpa/inet.h>
+#import <sys/socket.h>
+#import <sys/time.h>
+#import <sys/ioctl.h>
+#import <net/if.h>
+#import <fcntl.h>
+#import <netdb.h>
+#import <unistd.h>
+#import <net/route.h>
+#import <errno.h>
+
+
+#define DEFAULT_NUM_PENDING_CONNECTIONS (6)
+
+@implementation TFIPSocket
+
+@synthesize _socketBound, _socketConnected, _socketListening;
+
+- (id)init
+{
+	if (nil != (self = [super init])) {
+		_sockType = -1;		
+	}
+	
+	return self;
+}
+
+- (void)dealloc
+{	
+	[super dealloc];
+}
+
+- (BOOL)isConnectionOriented
+{
+	[self doesNotRecognizeSelector:_cmd];
+	return NO;
+}
+
+- (BOOL)close
+{
+	BOOL success = YES;
+	
+	[self unscheduleFromRunLoop];
+	
+	if ([self cfSocketCreated]) {
+		CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+		
+		if (-1 < bsdSocket)
+			success = (-1 < close(bsdSocket));
+		
+		CFSocketInvalidate(_cfSocketRef);
+		CFRelease(_cfSocketRef);
+		_cfSocketRef = NULL;
+	}
+	
+	_socketConnected = _socketListening = _socketBound = NO;
+	
+	return success;
+}
+
+- (BOOL)open
+{
+	if(![self cfSocketCreated]) {
+		CFSocketNativeHandle bsdSocket = socket(AF_INET, (int)_sockType, 0);
+		
+		if(bsdSocket < 0)
+			return NO;
+		
+		if (![self createCFSocketWithNativeHandle:bsdSocket]) {
+			(void)close(bsdSocket);
+			
+			return NO;
+		}
+	}
+	
+	return [self cfSocketCreated];
+}
+
+- (BOOL)bindToPort:(in_port_t)inPort atAddress:(in_addr_t)inAddr
+{	
+	if (![self cfSocketCreated] || [self isConnected] || [self isListening] || [self isBound])
+		return NO;
+	
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if(bsdSocket < 0)
+		return NO;
+	
+	// Set SO_REUSEADDR so we can reuse the address immediately
+	int socketOptionFlag = 1;
+	int result = setsockopt(bsdSocket, SOL_SOCKET, SO_REUSEADDR, &socketOptionFlag,
+							(socklen_t)sizeof(socketOptionFlag));
+	
+	if (result < 0)
+		return NO;
+	
+	struct sockaddr_in socketAddress;
+	bzero(&socketAddress, sizeof(socketAddress));
+	socketAddress.sin_family = PF_INET;
+	socketAddress.sin_addr.s_addr = inAddr;
+	socketAddress.sin_port = htons(inPort);
+	
+	result = bind(bsdSocket, (struct sockaddr*)&socketAddress, (socklen_t)sizeof(socketAddress));
+	if(result < 0)
+		return NO;
+	
+	_socketBound = YES;
+	
+	return YES;	
+}
+
+- (BOOL)connectTo:(in_addr_t)addr port:(in_port_t)port
+{
+	BOOL success = NO;
+
+	if([self cfSocketCreated] && (![self isConnected] || ![self isConnectionOriented]) && ![self isListening]) {
+		struct sockaddr_in socketAddress;
+	
+		bzero(&socketAddress, sizeof(socketAddress));
+		socketAddress.sin_addr.s_addr = addr;
+		socketAddress.sin_family = PF_INET;
+		socketAddress.sin_port = htons(port);
+		
+		// we connect using CFSocketConntectToAddress than via connect() on the native handle, so that
+		// we get the callback properly when the connection is made.
+		NSData* socketAddressData = [NSData dataWithBytes:(void*)&socketAddress length:sizeof(socketAddress)];
+		int socketError = CFSocketConnectToAddress(_cfSocketRef, (CFDataRef)socketAddressData, -1.0);
+		
+		success = (socketError != kCFSocketSuccess);
+	}
+	
+	return YES;
+}
+
+- (BOOL)listenOnPort:(in_port_t)inPort
+		   atAddress:(in_addr_t)inAddr
+maxPendingConnections:(NSUInteger)maxPendingConnections
+{	
+	if (![self cfSocketCreated] || [self isConnected] || [self isListening])
+		return NO;
+	
+	if (![self isBound] && ![self bindToPort:inPort atAddress:inAddr])
+		return NO;
+	
+	// we only need to call listen() for connection-oriented protocols 
+	if ([self isConnectionOriented] && ![self listenWithMaxPendingConnections:maxPendingConnections])
+		return NO;
+	
+	return YES;
+}
+
+- (BOOL)listenWithMaxPendingConnections:(int)maxPendingConnections
+{	
+	if(![self cfSocketCreated] || ![self isBound] || [self isConnected] || [self isListening])
+		return NO;
+	
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if(bsdSocket < 0)
+		return NO;
+	
+	int result = listen(bsdSocket, maxPendingConnections);
+	if(result < 0)
+		return NO;
+	
+	_socketListening = YES;
+	
+	return YES;
+}
+
+- (BOOL)resolveName:(NSString*)name intoAddress:(in_addr_t*)addrPtr
+{
+	in_addr_t			addr;
+	struct in_addr		inAddr;
+	const char*			nameString = [name cStringUsingEncoding:NSASCIIStringEncoding];
+	
+	// try these steps:
+	//   1) if it's an address in a.b.c.d form, parse it and return
+	//   2) if it's not an address, try to resolve it via DNS
+	//   3) if it's neither address nor hostname, interpret it as local network interface
+	//   4) fail
+	if (0 < inet_aton(nameString, &inAddr))
+		addr = inAddr.s_addr;
+	else {
+		struct hostent*	hostEntity;
+		
+		hostEntity = gethostbyname(nameString);
+		if(NULL != hostEntity) {
+			struct sockaddr_in	sAddr;
+			
+			bcopy((char*)hostEntity->h_addr, (char*)&sAddr.sin_addr, hostEntity->h_length);
+			addr = sAddr.sin_addr.s_addr;
+		} else {
+			struct ifreq			ifr;
+			struct sockaddr_in	*sin;
+			
+			bzero(&ifr, sizeof(struct ifreq));
+			strncpy(ifr.ifr_name, nameString, sizeof(ifr.ifr_name));
+			
+			sin = (struct sockaddr_in *)&ifr.ifr_addr;
+			sin->sin_family = AF_INET;
+			
+			if (ioctl([self nativeSocketHandle], SIOCGIFADDR, (char *)&ifr) < 0)
+				return NO;
+			
+			*sin = *((struct sockaddr_in*)&ifr.ifr_addr);
+			
+			addr = sin->sin_addr.s_addr;
+		}
+	}
+	
+	*addrPtr = addr;
+	
+	return YES;
+}
+
+- (BOOL)listenAt:(NSString*)name onPort:(in_port_t)port
+{
+	if (![self cfSocketCreated])
+		if (![self open])
+			return NO;
+	
+	in_addr_t addr = htonl(INADDR_ANY);
+	if (nil != name && ![self resolveName:name intoAddress:&addr])
+		return NO;
+	
+	return ([self listenOnPort:port atAddress:addr maxPendingConnections:DEFAULT_NUM_PENDING_CONNECTIONS]);
+}
+
+- (BOOL)connectTo:(NSString*)name atPort:(in_port_t)port
+{
+	if (![self cfSocketCreated])
+		if (![self open])
+			return NO;
+	
+	in_addr_t addr = htonl(INADDR_ANY);
+	if (nil != name && ![self resolveName:name intoAddress:&addr])
+		return NO;
+	
+	
+}
+
+- (BOOL)getPeerName:(struct sockaddr_in *)sockAddr
+{
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if(bsdSocket < 0 || NULL == sockAddr)
+		return NO;
+	
+	socklen_t addressLength = sizeof(struct sockaddr_in);
+	return (getpeername(bsdSocket, (struct sockaddr*)sockAddr, &addressLength) >= 0);
+}
+
+- (BOOL)getSockName:(struct sockaddr_in *)sockAddr
+{
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if(bsdSocket < 0 || NULL == sockAddr)
+		return NO;
+	
+	socklen_t addressLength = sizeof(struct sockaddr_in);
+	return (getsockname(bsdSocket, (struct sockaddr*)sockAddr, &addressLength) >= 0);
+}
+
+- (int)lastErrorCode
+{
+	return _lastErrorCode;
+}
+
+- (void)handleAvailableData
+{	
+	[self doesNotRecognizeSelector:_cmd];
+}
+
+- (void)handleConnectionEstablished
+{	
+	[self doesNotRecognizeSelector:_cmd];
+}
+
+- (void)handleDisconnection
+{
+	[self doesNotRecognizeSelector:_cmd];
+}
+
+- (void)handleNewConnection
+{
+	[self doesNotRecognizeSelector:_cmd];
+}
+
+- (void)handleWritableState
+{
+	[self doesNotRecognizeSelector:_cmd];
+}
+
+- (size_t)availableBytes
+{
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if (bsdSocket < 0)
+		return NO;
+	
+	size_t bytesAvailable = 0;
+	if (ioctl(bsdSocket, FIONREAD, &bytesAvailable) == -1) {
+		if (errno == EINVAL)
+			bytesAvailable = -1;
+		else
+			bytesAvailable = 0;
+	}
+	
+	return bytesAvailable;
+}
+
+- (int)receiveIntoBytes:(void*)bytes
+				 length:(size_t)length
+		   fromSockAddr:(struct sockaddr_in*)sockAddr
+{	
+	if (NULL == bytes || 0 == length)
+		return 0;
+	
+	size_t bytesAvailable = [self availableBytes];
+	size_t bufLen = (size_t)((0 == bytesAvailable) ? 1 : MIN(bytesAvailable, length));
+	
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if (bsdSocket < 0)
+		return 0;
+	
+	socklen_t sockLen = 0;
+	if (NULL != sockAddr) {
+		sockLen = sizeof(struct sockaddr);
+		memset(sockAddr, 0, (size_t)sockLen);
+	}
+	
+	int amountRead =
+		recvfrom(bsdSocket, bytes, bufLen, 0, (struct sockaddr*)sockAddr, &sockLen);
+	
+	if (0 > amountRead)
+		_lastErrorCode = errno;
+	
+	return amountRead;
+}
+
+- (int)sendBytes:(const void*)bytes
+		  length:(size_t)length
+	  toSockAddr:(const struct sockaddr_in*)sockAddr
+{
+	if (NULL == bytes || 0 == length)
+		return 0;
+	
+	CFSocketNativeHandle bsdSocket = [self nativeSocketHandle];
+	if (bsdSocket < 0)
+		return 0;
+	
+	socklen_t sockLen = 0;
+	if (NULL != sockAddr)
+		sockLen = sizeof(struct sockaddr_in);
+	
+	int amountSent = sendto(bsdSocket, bytes, length, 0, (const struct sockaddr*)sockAddr, sockLen);
+	if (length == amountSent) {
+		// We managed to write the entire outgoing buffer to the socket
+		// Disable the write callback for now since we don't need to know when we are writable again for now
+		CFSocketDisableCallBacks(_cfSocketRef, kCFSocketWriteCallBack);
+	} else if (0 <= amountSent) {
+		// We managed to write some of our buffer to the socket
+		// Enable the write callback on our CFSocketRef so we know when the socket is writable again
+		CFSocketEnableCallBacks(_cfSocketRef, kCFSocketWriteCallBack);
+	} else if (EWOULDBLOCK == errno) {
+		// No data has actually been written here
+		amountSent = 0;
+		_lastErrorCode = errno;
+		
+		// Enable the write callback on our CFSocketRef so we know when the socket is writable again
+		CFSocketEnableCallBacks(_cfSocketRef, kCFSocketWriteCallBack);
+	} else {
+		// Disable the write callback
+		CFSocketDisableCallBacks(_cfSocketRef, kCFSocketWriteCallBack);
+		
+		_lastErrorCode = errno;
+	}
+	
+	return amountSent;
+}
+
+@end

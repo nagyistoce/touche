6f1fbd53866ddb0f3954641211d31e535ce0f397
gkaindl <gkaindl@04bfa92c-8654-0410-99b0-a9ef281f400a>
2008-09-06 14:03:30 +0000

    Whoops, forgot the license disclaimer in the new files.

--- trunk/touche/src/TFTUIOTrackingDataDistributor.m
+++ trunk/touche/src/TFTUIOTrackingDataDistributor.m
@@ -1,9 +1,25 @@
 //
 //  TFTUIOTrackingDataDistributor.m
-//  Touche
+//  Touché
 //
 //  Created by Georg Kaindl on 6/9/08.
-//  Copyright 2008 Georg Kaindl. All rights reserved.
+//
+//  Copyright (C) 2008 Georg Kaindl
+//
+//  This file is part of Touché.
+//
+//  Touché is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  Touché is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with Touché. If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "TFTUIOTrackingDataDistributor.h"

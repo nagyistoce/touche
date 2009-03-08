54522fcacfb1de83a2c49fbd0c402c0d600f9fa8
gkaindl <gkaindl@04bfa92c-8654-0410-99b0-a9ef281f400a>
2009-03-08 22:17:12 +0000

    Updated license information on the source files of the Touchsmart TUIO project.

--- trunk/windows-tuio-bridge/src/TSNextwindowTouchInputSource.m
+++ trunk/windows-tuio-bridge/src/TSNextwindowTouchInputSource.m
@@ -3,7 +3,23 @@
 //  TouchsmartTUIO
 //
 //  Created by Georg Kaindl on 27/2/09.
-//  Copyright 2009 Georg Kaindl. All rights reserved.
+//
+//  Copyright (C) 2009 Georg Kaindl
+//
+//  This file is part of Touchsmart TUIO.
+//
+//  Touchsmart TUIO is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of
+//  the License, or (at your option) any later version.
+//
+//  Touchsmart TUIO is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with Touchsmart TUIO. If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "TSNextwindowTouchInputSource.h"
